e71f3ee51e41a18ca19ca16c05f93961642158f6
Yu Xiaoxian <self@yu-xiaoxian.me>
2020-04-09 16:20:06 +0800

    perapare ROS enviroment

--- action_ros_kinetic/entrypoint.sh
+++ action_ros_kinetic/entrypoint.sh
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# perapare
 ./opt/ros/kinetic/setup.sh
 
 source "/opt/ros/kinetic/setup.bash"
